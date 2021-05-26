614ea1c8cf9414134502f59ba4ff0241b051b1d4
yhoo0007 <yiping334@gmail.com>
2021-05-26 11:37:31 +0800

    Added Firebase config

--- config/shared.py
+++ config/shared.py
@@ -1,3 +1,5 @@
+import os
+
 
 LEAGUE = 'ultimatum'
 LEAGUE_CAP = 'Ultimatum'
@@ -9,3 +11,22 @@ DEFAULT_POE_HEADERS = {
 }
 
 DATA_DIR = f'./data/{LEAGUE}'
+
+FIREBASE_CONFIG = {
+        'apiKey': os.environ.get('apiKey'),
+        'authDomain': os.environ.get('authDomain'),
+        'databaseURL': os.environ.get('databaseURL'),
+        'storageBucket': os.environ.get('storageBucket'),
+        "serviceAccount": {
+                "type": os.environ.get('type'),
+                "project_id": os.environ.get('project_id'),
+                "private_key_id": os.environ.get('private_key_id'),
+                "private_key": os.environ.get('private_key'),
+                "client_email": os.environ.get('client_email'),
+                "client_id": os.environ.get('client_id'),
+                "auth_uri": os.environ.get('auth_uri'),
+                "token_uri": os.environ.get('token_uri'),
+                "auth_provider_x509_cert_url": os.environ.get('auth_provider_x509_cert_url'),
+                "client_x509_cert_url": os.environ.get('client_x509_cert_url'),
+            }
+    }
